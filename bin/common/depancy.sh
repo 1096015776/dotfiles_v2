3e86a955ddca591680e688157ab204a01bcbaa6a
fangxi <1096015776@qq.com>
2025-06-03 09:00:45 +0800

    add depancy shell

---
+++ bin/common/depancy.sh
@@ -0,0 +1,30 @@
+os=$(uname -s)
+common_pack=(
+tmux 
+nvim 
+lazygit 
+yazi
+fzf 
+ripgrep 
+fd
+jq
+git-delta
+zoxide
+bat
+eza
+prettyping
+imagemagick
+poppler
+ffmpeg
+fastfetch
+)
+
+mac_pack=(sevenzip)
+linux_pack=(7zip unzip zip)
+if [[ "$os" == "Darwin" ]]; then
+  source $ZDOTDIR/mac/init.zsh
+elif [[ "$os" == "Linux" ]]; then
+  source $ZDOTDIR/linux/init.zsh
+else
+fi
+
