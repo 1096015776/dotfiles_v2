98c545c56f0d198efa7724f66bd0c9cd90988230
fangxi <1096015776@qq.com>
2025-06-19 07:12:18 +0800

    update zsh some keymap

--- config/zsh/custom/custom_function.zsh
+++ config/zsh/custom/custom_function.zsh
@@ -8,12 +8,53 @@ function proxy_off(){
 }
 
 fzf-kill-process() {
-  local pid
-  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-
-  if [ "x$pid" != "x" ]; then
+  local pid=$(lsof -Pwni tcp | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:tcp]'" | awk '{print $2}')
+  if [ "x$pid" != "x" ]
+  then
     echo $pid | xargs kill -${1:-9}
+    ks
   fi
 }
+
 zle -N fzf-kill-process  # 注册为 Zsh 功能部件
 bindkey '^Z' fzf-kill-process  # 绑定 Ctrl+Z 到该功能
+
+find-in-file() {
+	grep --line-buffered --color=never -r "" * | fzf
+}
+
+fzf-kill-processt(){
+  local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+    kp
+  fi
+
+}
+
+zle -N fzf-kill-processt
+bindkey '^X' fzf-kill-processt
+
+zle -N find-in-file
+bindkey '^f' find-in-file
+
+function zle_eval {
+    echo -en "\e[2K\r"
+    eval "$@"
+    zle redisplay
+}
+
+function openlazygit {
+    zle_eval lazygit
+}
+
+zle -N openlazygit; bindkey "^G" openlazygit
+
+function opennvim {
+    zle_eval nvim
+}
+
+zle -N opennvim; bindkey "^N" opennvim
+
--- config/zsh/custom/init.zsh
+++ config/zsh/custom/init.zsh
@@ -1,5 +1,6 @@
+source $ZDOTDIR/custom/fzf.zsh
+source $ZDOTDIR/custom/vi.zsh
 source $ZDOTDIR/custom/env.zsh
 source $ZDOTDIR/custom/settings.zsh
 source $ZDOTDIR/custom/aliases.zsh
-source $ZDOTDIR/custom/fzf.zsh
 source $ZDOTDIR/custom/custom_function.zsh
