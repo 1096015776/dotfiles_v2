cc45ba875116dcbc11768de6a25cf17c307afd72
fangxi <1096015776@qq.com>
2025-07-25 14:52:57 +0800

    nvim add leetcode plugin setting cn to use

---
+++ config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    dependencies = {
+      -- include a picker of your choice, see picker section for more details
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      cn = { -- leetcode.cn
+        enabled = true, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+    },
+  },
+}
