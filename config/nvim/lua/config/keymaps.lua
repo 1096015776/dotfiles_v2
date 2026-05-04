d39a4362525c9584ab513c3ddf39563f215bc58c
fangxi <1096015776@qq.com>
2026-05-04 08:58:37 +0800

    fix: nvim use S to add surrond

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -74,4 +74,6 @@ end)
 --   vim.fn.setreg("+", vim.fn.expand("<cword>"))
 -- end, { silent = true })
 --
-vim.keymap.set("x","gs","<Plug>nvim-surround-visual",{desc="add a surrounding pair around a visual selection"})
+vim.keymap.set("x", "S", "<Plug>(nvim-surround-visual)", {
+  desc = "Add a surrounding pair around a visual selection",
+})
