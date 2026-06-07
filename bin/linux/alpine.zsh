86bd02b4827b2d21ae46a9581981a5fa41bb3fd3
fangxi <1096015776@qq.com>
2026-06-07 20:40:26 +0800

    alpine更新

--- bin/linux/alpine.zsh
+++ bin/linux/alpine.zsh
@@ -1,5 +1,6 @@
 # docker exec -it --detach-keys="ctrl-@,ctrl-@" fx-linux zsh
 # docker run -it --name fx-linux -e COLORTERM=truecolor -e TERM=xterm-256color --detach-keys="ctrl-@,ctrl-@" -w /root alpine:edge
+
 # apk add --no-cache ncurses-terminfo-base ncurses-terminfo ncurses-libs
 
 apk add --no-cache neovim lazygit fd ripgrep jq alpine-sdk delta tmux yazi fd zsh bash zip npm figlet nginx fzf lua-language-server stylua tree-sitter-cli fastfetch curl 7zip file bat eza openssh-client
--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -122,6 +122,9 @@ return {
     optional = true,
     dependencies = { "giuxtaposition/blink-cmp-copilot" },
     opts = {
+      -- fuzzy = {
+      --   implementation = "lua", -- 强制用 Lua，不报错
+      -- },
       sources = {
         default = { "copilot" },
         providers = {
